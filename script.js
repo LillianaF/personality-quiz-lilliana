ac0a92f7fe05c5ea2ea37079e24634bdb6b58b2f
Glitch (personality-quiz-starter-code) <none>
2020-08-05 15:31:42 +0000

    🚆🚴 Checkpoint ./script.js:19921051/2258 ./index.html:19921051/5585

--- script.js
+++ script.js
@@ -7,15 +7,39 @@ var result2Score = 0;
 var result3Score = 0;
 var result4Score = 0;
 
-
 //Take this quiz and find out what sibling order you are most like
 //Pick adjectives 
 //scenerios based on adjectives 
+
 //Your flight going home just got cancelled. What do you do?
 //Oldest-Immeadiately book the next flight
 //Middle-Wait around and hope for the best
 //Youngest-Stay on vacation longer
-//Only-Demand to speak with the head 
+//Only-Demand to speak with the head of the airline company
+
+//You are planning a picnic for your friends. What do you bring?
+//Oldest-Plan the location and bring a whole meal
+//Middle-Bring some snacks and hope no one is alergic to them
+//Youngest-Just brings themselves
+//Only-Only brings food for themselves
+
+//Your car breaks down in the road. What do you do?
+//Immediately call for help
+//Try to fix it yourself and then call for help
+//Fix it yourself
+//Cry 
+
+//boat crashes
+//find food shelter and plan to get off island
+//sit and hope for help
+//sets a home and plans to live there
+//cry
+
+//assignment due next week
+//does it right away
+//does it gradually throughout week
+//last minute
+//hire a tutor to do it for you
 
 
 //#TODO: Use the DOM to create variables for the first quiz question.
@@ -24,9 +48,40 @@ var q1a2 = document.getElementById("q1a2");
 var q1a3 = document.getElementById("q1a3");
 var q1a4 = document.getElementById("q1a4");
 
+var q2a1 = document.getElementById("q2a1");
+var q2a2 = document.getElementById("q2a2");
+var q2a3 = document.getElementById("q2a3");
+var q2a4 = document.getElementById("q2a4");
 
+var q3a1 = document.getElementById("q3a1");
+var q3a2 = document.getElementById("q3a2");
+var q3a3 = document.getElementById("q3a3");
+var q3a4 = document.getElementById("q3a4");
 
+var q4a1 = document.getElementById("q4a1");
+var q4a2 = document.getElementById("q4a2");
+var q4a3 = document.getElementById("q4a3");
+var q4a4 = document.getElementById("q4a4");
 
+var q5a1 = document.getElementById("q5a1");
+var q5a2 = document.getElementById("q5a2");
+var q5a3 = document.getElementById("q5a3");
+var q5a4 = document.getElementById("q5a4");
+
+var q6a1 = document.getElementById("q6a1");
+var q6a2 = document.getElementById("q6a2");
+var q6a3 = document.getElementById("q5a3");
+var q6a4 = document.getElementById("q5a4");
+
+var q5a1 = document.getElementById("q5a1");
+var q5a2 = document.getElementById("q5a2");
+var q5a3 = document.getElementById("q5a3");
+var q5a4 = document.getElementById("q5a4");
+
+var q5a1 = document.getElementById("q5a1");
+var q5a2 = document.getElementById("q5a2");
+var q5a3 = document.getElementById("q5a3");
+var q5a4 = document.getElementById("q5a4");
 
 
 //#TODO: Add Event Listeners to your answer choice variables.
