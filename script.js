5442f83a3536649eeeab064b5efc7deb7fafbd2e
Glitch (personality-quiz-starter-code) <none>
2020-08-05 15:55:21 +0000

    💰💓 Checkpoint ./index.html:19921051/177 ./script.js:19921051/1219

--- script.js
+++ script.js
@@ -2,44 +2,60 @@
 //This is where you will define the variables you will be using in your project.
 //#TODO: Create four variables to track each possible quiz outcome
 var questionCount = 0;
-var result1Score = 0;
-var result2Score = 0;
-var result3Score = 0;
-var result4Score = 0;
+var oldestChild = 0;
+var middleChild = 0;
+var youngestChild = 0;
+var onlyChild = 0;
 
 //Take this quiz and find out what sibling order you are most like
 //Pick adjectives 
 //scenerios based on adjectives 
 
+//1
 //Your flight going home just got canceled. What do you do?
 //Oldest-Immeadiately book the next flight
 //Middle-Wait around and hope for the best
 //Youngest-Stay on vacation longer
 //Only-Demand to speak with the head of the airline company
 
+//2
 //You are planning a picnic for your friends. What do you bring?
 //Oldest-Plan the location and bring a whole meal
 //Middle-Bring some snacks and hope no one is alergic to them
 //Youngest-Just brings themselves
 //Only-Only brings food for themselves
 
-//Your car breaks down in the middle of the road. What do you do?
-//Immediately call for help
-//Try to fix it yourself and then call for help
-//Fix it yourself
-//Cry 
+//3
+//You have an assignment due next week. When do you do it?
+//does it right away
+//does it gradually throughout week
+//last minute
 
+//4
 //Your boat crashes, leaving you stranded on an island. What do you do?
 //find food shelter and plan to get off island
 //sit and hope for help
 //sets a home and plans to live there
 //cry
 
-//assignment due next week
-//does it right away
-//does it gradually throughout week
-//last minute
+//5
+
+
+//6
 //hire a tutor to do it for you
+//Your car breaks down in the middle of the road. What do you do?
+//Immediately call for help
+//Try to fix it yourself and then call for help
+//Fix it yourself
+//Cry 
+
+//7
+//You are in a group project. What role do you take?
+//Takes charge of the entire project
+//Does all the work 
+//Does their work at the very last minute
+//Does their part, nothing more
+
 
 
 //#TODO: Use the DOM to create variables for the first quiz question.
